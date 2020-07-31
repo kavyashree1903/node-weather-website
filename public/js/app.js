7aff885f79f3f11d6a2208c66ae7244044486dd9
kavya <kavya.shree@venuemonk.com>
2020-07-31 18:24:02 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -19,14 +19,14 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "loading..";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
+  fetch("/weather?address=" + location).then(
     (response) => {
       response.json().then((data) => {
         if (data.error) {
           messageOne.textContent = data.error;
         } else {
           messageOne.textContent = data.location;
-          messageTwo.textContent = data.forecast;
+          messageTwo.textContent = forecast.relativeHumidity;
         }
       });
     }

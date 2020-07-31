60fdbb4b90553be5d2ce0a13b228214c431de124
kavya <kavya.shree@venuemonk.com>
2020-07-31 17:31:36 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,93 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Kavya Shree",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Kavya Shree",
+  });
+});
+
+//app.cpm
+//app.com/help
+//app.com/about
+
+app.get("/help/*", (req, res) => {
+  res.render("error", {
+    errorMessage: "Help article not found.",
+    title: "404",
+    name: "Kavya Shree",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location }={}) => {
+    if (error) {
+      return console.log(error);
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+      res.send({
+        address: req.query.address,
+        location: location,
+        forecast: forecastData,
+      });
+    });
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query); //returns the object we searched
+  res.send({
+    products: [],
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("error", {
+    errorMessage: "Page not found",
+    title: "404",
+    name: "Kavya Shree",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000!");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const forecast = (lon, lat, callback) => {
+  const url =
+    "https://api.weatherbit.io/v2.0/current?lat=" +
+    encodeURIComponent(lat) +
+    "&lon=" +
+    encodeURIComponent(lon) +
+    "&key=8ce5d1deb9f74a5ca5b4636f97924f89";
+
+  request({ url, json: true }, (error, {body}) => {
+    if (error) {
+      callback("Unable to connect forecast services!", undefined);
+    } else if (body.error) {
+      callback("Unable to find the location.", undefined);
+    } else {
+      callback(undefined, {
+        relativeHumidity: body.data[0].rh,
+      });
+    }
+  });
+};
+
+module.exports = forecast;
